6a7ec3ee0260e93b267230e44e67fd86551ee06b
Daryna Karmazin <daryna.karmazin@gmail.com>
2023-07-05 18:27:26 +0300

    Updated styles to mob.

--- src/components/App.styled.jsx
+++ src/components/App.styled.jsx
@@ -3,7 +3,7 @@ import styled from 'styled-components';
 export const Wrapper = styled.div`
   @media screen and (max-width: 767px) {
     width: 100%;
-    height: 100%;
+    height: vh;
   }
 
   display: flex;
