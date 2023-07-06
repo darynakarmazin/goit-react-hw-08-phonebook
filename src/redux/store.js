b8a135713c050e0c82378421ecc6c0aef72688b6
Daryna Karmazin <daryna.karmazin@gmail.com>
2023-07-06 18:52:47 +0300

    Updated slice

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,7 +1,7 @@
 import { configureStore } from '@reduxjs/toolkit';
 import { contactsReducer } from './contacts/contactsSlice';
 import { filterReducer } from './contacts/filterSlice';
-import { authReducer } from './auth/slice';
+import { authReducer } from './auth/authSlice';
 
 export const store = configureStore({
   reducer: {
