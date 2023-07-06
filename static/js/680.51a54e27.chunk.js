40801658b16750e96471516bee8a9be3e8866cc3
darynakarmazin <daryna.karmazin@gmail.com>
2023-07-06 20:04:17 +0000

    Deploying to gh-pages from @ darynakarmazin/goit-react-hw-08-phonebook@7d2358b29ad3e0140c8f47fd8b4bf2e9fb227361 🚀

---
+++ static/js/680.51a54e27.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkreact_homework_template=self.webpackChunkreact_homework_template||[]).push([[680],{680:function(e,t,n){n.r(t),n.d(t,{default:function(){return c}});var r=n(2606),o=n(184);function c(){return(0,o.jsx)(o.Fragment,{children:(0,o.jsx)(r.h,{children:"Welcome to Phonebook!"})})}}}]);
+//# sourceMappingURL=680.51a54e27.chunk.js.map
