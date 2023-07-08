8ad2cc399c1ec28448901307f9bab38bc50bcfe7
Daryna Karmazin <daryna.karmazin@gmail.com>
2023-07-08 10:07:47 +0300

    Updated controlled forms

--- src/pages/LoginPage/LoginPage.jsx
+++ src/pages/LoginPage/LoginPage.jsx
@@ -2,17 +2,36 @@ import { Header } from 'components/App.styled';
 import { useDispatch } from 'react-redux';
 import { logIn } from 'redux/auth/operations';
 import { Input, Form, Button } from 'components/contactForm/ContactForm.styled';
+import { useState } from 'react';
 
 export default function Login() {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+
   const dispatch = useDispatch();
+
+  const handleChange = event => {
+    const { name, value } = event.target;
+
+    switch (name) {
+      case 'email':
+        setEmail(value);
+        break;
+      case 'password':
+        setPassword(value);
+        break;
+      default:
+        return;
+    }
+  };
 
   const handleSubmit = e => {
     e.preventDefault();
     const form = e.currentTarget;
     dispatch(
       logIn({
-        email: form.elements.email.value,
-        password: form.elements.password.value,
+        email,
+        password,
       })
     );
     form.reset();
@@ -25,11 +44,21 @@ export default function Login() {
       <Form onSubmit={handleSubmit}>
         <label>
           Email
-          <Input type="email" name="email" />
+          <Input
+            type="email"
+            name="email"
+            value={email}
+            onChange={handleChange}
+          />
         </label>
         <label>
           Password
-          <Input type="password" name="password" />
+          <Input
+            type="password"
+            name="password"
+            value={password}
+            onChange={handleChange}
+          />
         </label>
         <Button type="submit">Log In</Button>
       </Form>
--- src/pages/RegisterPage/RegisterPage.jsx
+++ src/pages/RegisterPage/RegisterPage.jsx
@@ -2,9 +2,32 @@ import { Header } from 'components/App.styled';
 import { useDispatch } from 'react-redux';
 import { register } from 'redux/auth/operations';
 import { Input, Form, Button } from 'components/contactForm/ContactForm.styled';
+import { useState } from 'react';
 
 export default function RegisterPage() {
+  const [name, setName] = useState('');
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+
   const dispatch = useDispatch();
+
+  const handleChange = event => {
+    const { name, value } = event.target;
+
+    switch (name) {
+      case 'name':
+        setName(value);
+        break;
+      case 'email':
+        setEmail(value);
+        break;
+      case 'password':
+        setPassword(value);
+        break;
+      default:
+        return;
+    }
+  };
 
   const handleSubmit = e => {
     e.preventDefault();
@@ -25,15 +48,25 @@ export default function RegisterPage() {
       <Form onSubmit={handleSubmit}>
         <label>
           Username
-          <Input type="text" name="name" />
+          <Input type="text" name="name" value={name} onChange={handleChange} />
         </label>
         <label>
           Email
-          <Input type="email" name="email" />
+          <Input
+            type="email"
+            name="email"
+            value={email}
+            onChange={handleChange}
+          />
         </label>
         <label>
           Password
-          <Input type="password" name="password" />
+          <Input
+            type="password"
+            name="password"
+            value={password}
+            onChange={handleChange}
+          />
         </label>
         <Button type="submit">Register</Button>
       </Form>
